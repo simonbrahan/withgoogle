7a8f58f698e58c2afa208cacba2a2f65786db361
Simon Brahan <simon.brahan@frogeducation.com>
2019-01-25 15:54:18 +0000

    Optimised max_span - now only needs one pass over the list

--- max_span.py
+++ max_span.py
@@ -1,15 +1,24 @@
 def max_span(nums):
-    spans = {}
+    idxes = {}
+    span_lengths = {}
+    max_span_length = 1
 
     for idx, num in enumerate(nums):
-        if num not in spans:
-            spans[num] = [idx-1, idx]
+        if num not in idxes:
+            # If this is the first instance of num, store its index and a span length of 1
+            span_lengths[num] = 1
         else:
-            spans[num][1] = idx
+            # Otherwise, update the span length
+            span_lengths[num] = span_lengths[num] - idxes[num] + idx
 
-    span_sizes = map(lambda idxes: idxes[1] - idxes[0], spans.values())
+            # If a new span is found, check against the cached max and update if needed
+            if span_lengths[num] > max_span_length:
+                max_span_length = span_lengths[num]
 
-    return max(span_sizes)
+        # Regardless, a new index for num has been found so store it
+        idxes[num] = idx
+
+    return max_span_length
 
 
 print max_span([1, 2, 1, 1, 3]), 'should be 4'
