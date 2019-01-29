2e46eed8e85cb82912821acb01e148421e075e8f
Simon Brahan <simon.brahan@frogeducation.com>
2019-01-29 15:07:27 +0000

    Hangman program now plays correctly for hard coded word

--- hangman.py
+++ hangman.py
@@ -1,3 +1,6 @@
+answer = "FUZZY"
+max_incorrect_guesses = 8
+num_incorrect_guesses = 0
 guesses = set()
 
 def get_guess():
@@ -18,7 +21,39 @@ def get_guess():
         return normalised_guess
 
 
+def get_hint():
+    output = ""
+
+    for char in answer:
+        if char in guesses:
+            output += char
+        else:
+            output += "-"
+
+    return output
+
+
 while True:
+    print "Word looks like %s" % (get_hint(), )
+
+    if "-" not in get_hint():
+        print "You win"
+        break
+
     guess = get_guess()
-    print "you guessed: ", guess
+    print "you guessed: %s" % (guess, )
+
+    if guess not in answer:
+        num_incorrect_guesses += 1
+        print "Wrong guess"
+
+        if num_incorrect_guesses >= max_incorrect_guesses:
+            print "You have lost"
+            break
+        else:
+            print "You have %d guesses remaining" % (max_incorrect_guesses - num_incorrect_guesses)
+            continue
+
+    print "Good guess"
+
 
